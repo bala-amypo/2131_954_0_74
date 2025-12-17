58ab4848e49c50b7eb582fd608c8451537e75012
bala-amypo <bala@amypo.in>
2025-12-17 04:37:39 +0000

    User commit

--- src/main/java/com/example/demo/Controller/Studctl.java
+++ src/main/java/com/example/demo/Controller/Studctl.java
@@ -3,4 +3,6 @@ import java.util.list;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation
